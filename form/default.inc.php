0c4397e27d0c48979df08c220244aae5371127a6
“Allie <allieroll22@ufl.edu>
2018-07-27 16:45:15 -0400

    Edited default error page and styled

--- form/default.inc.php
+++ form/default.inc.php
@@ -1,2 +1,24 @@
-<h1>Hello</h1>
-<p>Form processor by <a href="http://www.inventpartners.com">Invent Partners Web Design York</a>.</p>
+<style type="text/css">
+
+body {
+  background-image: url("../images/background.png");
+  background-attachment: fixed;
+  background-position: center;
+  text-align: center;
+}
+
+h1 {
+  font-size: 30px;
+  color: white;
+}
+
+p {
+  font-size: 25px;
+  color: white;
+}
+</style>
+
+<body>
+  <h1>Sorry, something went wrong with your submission</h1>
+  <p>Please try again</p>
+</body>inventpartners.com">Invent Partners Web Design York</a>.</p>
