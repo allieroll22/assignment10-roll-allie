39a551d0861c5db2598f0913596f79f494145dfc
“Allie <allieroll22@ufl.edu>
2018-07-27 16:51:39 -0400

    Edited Thank You page

--- form/thankyou.inc.php
+++ form/thankyou.inc.php
@@ -1,3 +1,24 @@
-<h1>Thank you for your enquiry</h1>
-<p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
-<p>Form processor provided by <a href="http://www.inventpartners.com">Invent Partners web design York</a>.</p>
+<style type="text/css">
+
+body {
+  background-image: url("../images/background.png");
+  background-attachment: fixed;
+  background-position: center;
+  text-align: center;
+}
+
+h1 {
+  font-size: 30px;
+  color: white;
+}
+
+p {
+  font-size: 25px;
+  color: white;
+}
+</style>
+
+<body>
+  <h1>Thank you for your enquiry</h1>
+  <p>Thanks for your interest. we have received your message, and we will be in touch shortly.</p>
+</body>
